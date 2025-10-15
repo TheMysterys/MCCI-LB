e0d6795d7e6a427b515789a8ded8c44917a2e0a8
TheMysterys <the-mystery@outlook.com>
2025-10-15 21:51:03 +1300

    Create main tables

--- create_tables.js
+++ create_tables.js
@@ -9,16 +9,25 @@ const client = createClient({
 	database: "mcci",
 });
 
+const mainTables = [
+	"trophies",
+	"trophies_skill",
+	"trophies_style",
+	"trophies_bonus",
+	"trophies_fishing",
+	"royal_reputation",
+];
+
 async function createGameTable(game) {
 	let table_name = `leaderboard_${game}`;
 	let ddl = `
 	CREATE TABLE IF NOT EXISTS ${table_name} (
-        ts          DateTime DEFAULT now(),
+        ts          DateTime('UTC') DEFAULT now(),
         statistic 	String,
         uuid 		Nullable(String),
         username    Nullable(String),
         ranks       Array(String),
-        value       Int64,
+        Value       Int64,
         rank        Int32
     )
     ENGINE = MergeTree
@@ -32,11 +41,33 @@ async function createGameTable(game) {
 	log(`Ensured table exists: ${table_name}`, LogType.SUCCESS);
 }
 
+async function createMainTable(stat) {
+	let table_name = `leaderboard_${stat}`;
+	let ddl = `
+	CREATE TABLE IF NOT EXISTS ${table_name} (
+        ts          DateTime('UTC') DEFAULT now(),
+        uuid 		String,
+        username    String,
+        ranks       Array(String),
+        Value       Int64,
+        rank        Int32
+    )
+    ENGINE = MergeTree
+    PARTITION BY toYYYYMMDD(ts)
+    ORDER BY (ts, rank)
+	`;
+
+	await client.exec({
+		query: ddl,
+	});
+	log(`Ensured table exists: ${table_name}`, LogType.SUCCESS);
+}
+
 async function createFactionTable() {
 	let table_name = `leaderboard_factions`;
 	let ddl = `
 	CREATE TABLE IF NOT EXISTS ${table_name} (
-        ts          DateTime DEFAULT now(),
+        ts          DateTime('UTC') DEFAULT now(),
         faction     String, 
         value       Int32,
         rank        Int32
@@ -56,7 +87,7 @@ async function createFactionPlayersTable() {
 	let table_name = `leaderboard_factions_players`;
 	let ddl = `
 	CREATE TABLE IF NOT EXISTS ${table_name} (
-        ts         DateTime DEFAULT now(),
+        ts         DateTime('UTC') DEFAULT now(),
         faction    String,
         uuid       String,
         username   String,
@@ -78,12 +109,15 @@ async function createFactionPlayersTable() {
 async function main() {
 	log("Connected to Clickhouse", LogType.NETWORK);
 
-	for (let game in leaderboards) {
+	for (const game in leaderboards) {
 		await createGameTable(game);
 	}
-	await createFactionTable()
-	await createFactionPlayersTable()
-	await client.close()
+	for (const stat of mainTables) {
+		await createMainTable(stat);
+	}
+	await createFactionTable();
+	await createFactionPlayersTable();
+	await client.close();
 }
 
 main();
